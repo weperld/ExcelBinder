96a9409dc1ea1bd6faf06088259845ca34cb1a5d
박형준 <weperlw@gmail.com>
2026-01-26 14:06:16 +0900

    [refactor] 대시보드 아이템 템플릿 셀렉터 도입

---
+++ ExcelBinder/Views/DashboardItemTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ExcelBinder.Models;
+
+namespace ExcelBinder.Views
+{
+    public class DashboardItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate? StaticDataTemplate { get; set; }
+        public DataTemplate? LogicTemplate { get; set; }
+        public DataTemplate? SchemaGenTemplate { get; set; }
+
+        public override DataTemplate? SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is FeatureDefinition feature)
+            {
+                return feature.Category switch
+                {
+                    ProjectConstants.Categories.StaticData => StaticDataTemplate,
+                    ProjectConstants.Categories.Logic => LogicTemplate,
+                    ProjectConstants.Categories.SchemaGen => SchemaGenTemplate,
+                    _ => base.SelectTemplate(item, container)
+                };
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
